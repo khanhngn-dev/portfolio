a545baf0fe6b6218a641c9292528d295ae518f28
Khanh Nguyen <khanhngn.dev@gmail.com>
2023-12-14 05:57:19 +0000

    Remove fonts

--- src/constants/fonts.ts
+++ src/constants/fonts.ts
@@ -1,6 +1,4 @@
 import { Nunito } from 'next/font/google';
-import { Maven_Pro } from 'next/font/google';
 export { GeistMono } from 'geist/font/mono';
 export { GeistSans } from 'geist/font/sans';
 export const nunito = Nunito({ subsets: ['vietnamese'] });
-export const maven_pro = Maven_Pro({ subsets: ['vietnamese'] });
