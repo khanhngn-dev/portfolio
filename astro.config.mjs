aabc84e0eb58db433ba1fbf226336a598777cf8e
Khanh Nguyen <khanhngn.dev@gmail.com>
2025-06-01 00:45:12 +0700

    chore: update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import vercel from '@astrojs/vercel';
-
 import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   adapter: vercel(),
-  output: 'server',
+  output: 'static',
   vite: {
     plugins: [tailwindcss()],
   },
