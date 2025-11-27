add01f93526dac9eabb518df4456829a7d37f202
Khanh Nguyen <khanhngn.dev@gmail.com>
2025-11-27 15:14:03 +0700

    feat: update navbar

--- src/components/features/inquiry-form.tsx
+++ src/components/features/inquiry-form.tsx
@@ -1,11 +1,4 @@
 import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
@@ -39,78 +32,66 @@ const InquiryForm = () => {
   };
 
   return (
-    <Card className="max-w-2xl w-full mx-auto">
-      <CardHeader>
-        <CardTitle>Left your inquiries here</CardTitle>
-        <CardDescription>
-          What would you like to discuss? Feel free to reach out!
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form
-          className="flex flex-col gap-4"
-          onSubmit={form.handleSubmit(onSubmit)}
-        >
-          <div className="flex flex-col gap-2">
-            <Label>Name</Label>
-            <Input
-              className="text-sm"
-              type="text"
-              autoComplete="name"
-              placeholder="John Doe"
-              {...form.register('name', {
-                required: true,
-              })}
-              aria-invalid={!!form.formState.errors.name}
-            />
-            {form.formState.errors.name && (
-              <span className="text-sm text-destructive">Name is required</span>
-            )}
-          </div>
-          <div className="flex flex-col gap-2">
-            <Label>Email</Label>
-            <Input
-              className="text-sm"
-              name="email"
-              type="email"
-              autoComplete="email"
-              placeholder="john.doe@example.com"
-              {...form.register('email', {
-                required: true,
-                pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.]+\.[a-zA-Z]{2,}$/,
-              })}
-              aria-invalid={!!form.formState.errors.email}
-            />
-            {form.formState.errors.email && (
-              <span className="text-sm text-destructive">
-                Please enter a valid email address
-              </span>
-            )}
-          </div>
-          <div className="flex flex-col gap-2">
-            <Label>Message</Label>
-            <Textarea
-              className="text-sm"
-              name="message"
-              placeholder="Anything really..."
-              {...form.register('message', {
-                required: true,
-              })}
-              aria-invalid={!!form.formState.errors.message}
-            />
-            {form.formState.errors.message && (
-              <span className="text-sm text-destructive">
-                Message is required
-              </span>
-            )}
-          </div>
-          <Button className="sm:self-end" type="submit" disabled={isPending}>
-            Send
-            <SendIcon />
-          </Button>
-        </form>
-      </CardContent>
-    </Card>
+    <form
+      className="flex flex-col gap-4"
+      onSubmit={form.handleSubmit(onSubmit)}
+    >
+      <div className="flex flex-col gap-2">
+        <Label>Name</Label>
+        <Input
+          className="text-sm"
+          type="text"
+          autoComplete="name"
+          placeholder="John Doe"
+          {...form.register('name', {
+            required: true,
+          })}
+          aria-invalid={!!form.formState.errors.name}
+        />
+        {form.formState.errors.name && (
+          <span className="text-sm text-destructive">Name is required</span>
+        )}
+      </div>
+      <div className="flex flex-col gap-2">
+        <Label>Email</Label>
+        <Input
+          className="text-sm"
+          name="email"
+          type="email"
+          autoComplete="email"
+          placeholder="john.doe@example.com"
+          {...form.register('email', {
+            required: true,
+            pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.]+\.[a-zA-Z]{2,}$/,
+          })}
+          aria-invalid={!!form.formState.errors.email}
+        />
+        {form.formState.errors.email && (
+          <span className="text-sm text-destructive">
+            Please enter a valid email address
+          </span>
+        )}
+      </div>
+      <div className="flex flex-col gap-2">
+        <Label>Message</Label>
+        <Textarea
+          className="text-sm"
+          name="message"
+          placeholder="Anything really..."
+          {...form.register('message', {
+            required: true,
+          })}
+          aria-invalid={!!form.formState.errors.message}
+        />
+        {form.formState.errors.message && (
+          <span className="text-sm text-destructive">Message is required</span>
+        )}
+      </div>
+      <Button className="sm:self-end" type="submit" disabled={isPending}>
+        Send
+        <SendIcon />
+      </Button>
+    </form>
   );
 };
 
--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,10 +1,7 @@
+import GithubIcon from '@/components/icons/github';
 import { saveTheme } from '@/libs/theme';
 import { MoonIcon, SunIcon } from 'lucide-react';
-import GithubIcon from '../icons/github';
 import { Button } from './button';
-import { NavigationMenu } from './navigation-menu';
-
-const links = [];
 
 const Navbar = () => {
   const onToggleTheme = () => {
@@ -22,76 +19,26 @@ const Navbar = () => {
     }
   };
 
-  const onHashLinkClick = (
-    e: React.MouseEvent<HTMLAnchorElement>,
-    href: string,
-  ) => {
-    e.preventDefault();
-    const id = href.slice(href.indexOf('#') + 1);
-    const element = document.getElementById(id);
-    if (element) {
-      element.scrollIntoView({ behavior: 'smooth', block: 'center' });
-    }
-  };
-
   return (
     <div className="fixed top-4 left-1/2 -translate-x-1/2 max-w-4xl w-full px-4 z-10">
-      <NavigationMenu
-        viewport={false}
-        className="bg-background/80 backdrop-blur-sm border p-1.5 rounded-lg w-full max-w-full"
-      >
-        {/* <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button className="flex sm:hidden" variant="ghost" size="icon">
-              <MenuIcon />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-[calc(var(--radix-popper-available-width)-2rem)] mx-4 mt-2 bg-background/80 backdrop-blur-sm">
-            <div className="flex flex-col gap-2">
-              {links.map(({ href, title }) => (
-                <DropdownMenuItem key={href} asChild>
-                  <a href={href} onClick={(e) => onHashLinkClick(e, href)}>
-                    {title}
-                  </a>
-                </DropdownMenuItem>
-              ))}
-            </div>
-          </DropdownMenuContent>
-        </DropdownMenu> */}
+      <nav className="relative flex flex-1 items-center bg-background/80 backdrop-blur-sm border-2 p-1 rounded-lg w-full max-w-full">
         <div className="px-3">
           <a href="/" className="font-semibold" onClick={onHomeClick}>
             khanhngn
           </a>
         </div>
-        {/* <NavigationMenuList className="gap-4 w-full flex-1 hidden sm:flex">
-          {links.map(({ href, title }) => {
-            return (
-              <NavigationMenuItem key={href}>
-                <NavigationMenuLink
-                  href={href}
-                  onClick={(e) => onHashLinkClick(e, href)}
-                >
-                  {title}
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-            );
-          })}
-        </NavigationMenuList> */}
         <div className="flex-1 flex justify-end items-center gap-1">
-          {/* <Button variant="ghost" size="icon" onClick={openSearchCommand}>
-            <SearchIcon />
-          </Button> */}
           <Button size="icon" variant="ghost" onClick={onToggleTheme}>
             <SunIcon className="dark:hidden" />
             <MoonIcon className="hidden dark:flex" />
           </Button>
-          <Button variant="ghost" asChild>
-            <a href="https://github.com/khanhngn-dev" target="_blank">
+          <a href="https://github.com/khanhngn-dev" target="_blank">
+            <Button size="icon" variant="ghost">
               <GithubIcon />
-            </a>
-          </Button>
+            </Button>
+          </a>
         </div>
-      </NavigationMenu>
+      </nav>
     </div>
   );
 };
